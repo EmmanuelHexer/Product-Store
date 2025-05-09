a113887034c2ca7857a38e1e476a0da72a5c1b19
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-05-09 02:38:15 +0000

    setting up server and connecting mongo db database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Server ready");
 });
 
 app.listen(5000, () => {
+  connectDb();
   console.log(`server start at http://localhost:5000`);
 });
