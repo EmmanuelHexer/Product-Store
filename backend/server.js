6abc837fc9a08af9c81782dab1ebc75da703dc58
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-05-10 08:57:23 +0000

    create product endpoint and deleteproduct endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,41 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server ready");
+app.use(express.json()); // allows us to accept json data in the req.body
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in create product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    console.log("id:", id);
+  } catch (error) {
+    console.log("Error:", Error);
+  }
 });
 
 app.listen(5000, () => {
