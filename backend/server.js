0c9b6cd91812d29ece60d94509bb9138e6e9d824
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-05-11 01:58:52 +0000

    Finished with api controllers and initialized the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
-import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept json data in the req.body
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user will send this data
+app.use("/api/products", productRoutes);
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in create product", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    console.log("id:", id);
-  } catch (error) {
-    console.log("Error:", Error);
-  }
-});
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDb();
   console.log(`server start at http://localhost:5000`);
 });
